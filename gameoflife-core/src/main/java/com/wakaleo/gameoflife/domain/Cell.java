275913b89b0ec685cbe6fdf3d006790bcb71418c
EC2 Default User <ec2-user@ip-172-31-9-64.ec2.internal>
2017-11-02 07:25:46 +0000

    remote changes

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -6,7 +6,6 @@ package com.wakaleo.gameoflife.domain;
  *
  * Cell.java
  * Cell object class, contains information on individual cells (positions) in the grid
- * small change test
  */
 public enum Cell {
 	// Symbols to represent cell status
